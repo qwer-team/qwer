f8826576b28446daf99bc04dfa1eabbdebddb3d2
root <root@pcuser.(none)>
2012-11-28 17:55:21 +0200

    lala

---
+++ Controller/DefaultController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Main\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Itc\AdminBundle\Tools\LanguageHelper;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $locale =  LanguageHelper::getLocale();
+        
+        $queryBuilder = $em->getRepository('ItcAdminBundle:Menu\Menu')
+                        ->createQueryBuilder('M')
+                        ->select( 'M, T' )
+                        ->leftJoin('M.translations', 'T',
+                                'WITH', "T.locale = :locale")
+                        ->orderBy('M.kod', 'ASC')
+                        ->setParameter('locale', $locale);
+
+        $entities = $queryBuilder->getQuery()->execute();
+        $entity = $entities[0];
+        
+        $galleries = $entity->getGalleries();
+        $images    = $galleries[0]->getImages();
+        
+        return array( 
+            'entity' => $entity,
+            'images' => $images,
+        );
+        return array( 
+            'entities'   => $entities,
+            'entity' => $entity,
+            'images' => $images,
+        );
+    }
+    /**     
+     * Lists all Menu entities.
+     * @Template()
+     */
+    public function menuAction(){
+
+        $em = $this->getDoctrine()->getManager();
+        $locale =  LanguageHelper::getLocale();
+        
+        $queryBuilder = $em->getRepository('ItcAdminBundle:Menu\Menu')
+                        ->createQueryBuilder('M')
+                        ->select( 'M, T' )
+                        ->leftJoin('M.translations', 'T',
+                                'WITH', "T.locale = :locale")
+                        ->orderBy('M.kod', 'ASC')
+                        ->setParameter('locale', $locale);
+
+        $arr = $queryBuilder->getQuery()->execute();
+        
+        $entities = $child_entities = array();
+
+        foreach($arr as $v)
+            if (is_null($v->getParentId()) )
+                $entities[] = $v;
+                
+        return array( 
+            "entities"       => $entities,
+            "locale"         => $locale,
+        );
+    }
+    
+}
